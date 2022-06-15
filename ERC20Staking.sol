e5f8b90a7d47f0117ca1b14bb2d20746f0205b4a
nstanogias <nstanogias@gmail.com>
2022-06-15 10:26:33 +0300

    impl

---
+++ ERC20Staking.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error TransferFailed();
+error NeedsMoreThanZero();
+
+contract Staking14 is ReentrancyGuard, Ownable {
+    IERC20 public s_rewardsToken;
+    IERC20 public s_stakingToken;
+
+    // This is the reward token per minute
+    // Which will be multiplied by the tokens the user staked divided by the total
+    // This is a steady reward rate of the platform
+    // That means that the more users stake, the less the reward is for everyone who is staking.
+    uint256 public constant REWARD_RATE = 100;
+    uint256 public s_lastUpdateTime;
+    uint256 public s_rewardPerTokenStored;
+    uint256 private s_totalSupply;
+
+    mapping(address => uint256) public s_userRewardPerTokenPaid;
+    mapping(address => uint256) public s_rewards;
+    mapping(address => uint256) public s_stakes;
+
+    event Staked(address indexed user, uint256 indexed amount);
+    event WithdrewStake(address indexed user, uint256 indexed amount);
+    event RewardsClaimed(address indexed user, uint256 indexed amount);
+
+    constructor(address stakingToken, address rewardsToken) {
+        s_stakingToken = IERC20(stakingToken);
+        s_rewardsToken = IERC20(rewardsToken);
+    }
+
+    /**
+     * @notice How much reward a token gets based on how long it's been in and during which "snapshots"
+     */
+    function rewardPerToken() public view returns (uint256) {
+        if (s_totalSupply == 0) {
+            return s_rewardPerTokenStored;
+        }
+        return
+            s_rewardPerTokenStored +
+            (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalSupply);
+    }
+
+    /**
+     * @notice How much reward a user has earned
+     */
+    function earned(address account) public view returns (uint256) {
+        return
+            ((s_stakes[account] * (rewardPerToken() - s_userRewardPerTokenPaid[account])) / 1e18) + s_rewards[account];
+    }
+
+    /**
+     * @notice Deposit tokens into this contract
+     * @param amount | How much to stake
+     */
+    function stake(uint256 amount)
+        external
+        updateReward(msg.sender)
+        nonReentrant
+        moreThanZero(amount)
+    {
+        s_totalSupply += amount;
+        s_stakes[msg.sender] += amount;
+        emit Staked(msg.sender, amount);
+        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    /**
+     * @notice Withdraw tokens from this contract
+     * @param amount | How much to withdraw
+     */
+    function withdraw(uint256 amount) external updateReward(msg.sender) nonReentrant {
+        s_totalSupply -= amount;
+        s_stakes[msg.sender] -= amount;
+        emit WithdrewStake(msg.sender, amount);
+        bool success = s_stakingToken.transfer(msg.sender, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    /**
+     * @notice User claims their tokens
+     */
+    function claimReward() external updateReward(msg.sender) nonReentrant {
+        uint256 reward = s_rewards[msg.sender];
+        s_rewards[msg.sender] = 0;
+        emit RewardsClaimed(msg.sender, reward);
+        bool success = s_rewardsToken.transfer(msg.sender, reward);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    /********************/
+    /* Modifiers Functions */
+    /********************/
+    modifier updateReward(address account) {
+        s_rewardPerTokenStored = rewardPerToken();
+        s_lastUpdateTime = block.timestamp;
+        s_rewards[account] = earned(account);
+        s_userRewardPerTokenPaid[account] = s_rewardPerTokenStored;
+        _;
+    }
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    /********************/
+    /* Getter Functions */
+    /********************/
+    function getStaked(address _stakeholder) public view returns (uint256) {
+        return s_stakes[_stakeholder];
+    }
+
+    function getStakableTokensBalance(address account) public view returns (uint256) {
+      return s_stakingToken.balanceOf(account);
+    }
+
+    function getRewardsTokensBalance(address account) public view returns (uint256) {
+      return s_rewardsToken.balanceOf(account);
+    }
+
+    function getTotalRewardTokensSupply() public view returns (uint256) {
+      return s_rewardsToken.totalSupply();
+    }
+
+    function getTotalStakingTokensSupply() public view returns (uint256) {
+      return s_stakingToken.totalSupply();
+    }
+
+    function getTotalStakes() public view returns (uint256) {
+      return s_totalSupply;
+    }
+
+    function getRewardPerTokenStored() public view returns (uint256) {
+      return s_rewardPerTokenStored;
+    }
+
+    function getSender() public view returns (address) {
+      return msg.sender;
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,159 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useContract, useDisconnect, useMetamask, useTokenDrop } from '@thirdweb-dev/react';
+import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+import { useSigner, useToken, useTokenBalance } from '@thirdweb-dev/react';
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import { BigNumber, ethers } from 'ethers';
+import Spinner from '../components/Spinner/Spinner';
+
+const tokenDropContractAddress = '0x196314F89A06bC1E7d7B161a92317bCed56e0d77';
+const tokenRewardContractAddress = '0xF1776ad3ddA88aA52c5291E53952A987E917c2D0';
+const stakingContractAddress = '0xb40d3A314E99c4260da389B50FF45E3b8De70702';
+
+const style = {
+  button:
+    'border border-[#282b2f] bg-[#8a9ad1] p-[0.5rem] my-2 text-xl font-semibold rounded-lg cursor-pointer text-black w-[200px]',
+};
 
 const Home: NextPage = () => {
+  // Wallet Connection Hooks
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const disconnectWallet = useDisconnect();
+
+  // Contract Hooks
+  const tokenDropContract = useTokenDrop(tokenDropContractAddress);
+  const rewardTokenContract = useToken(tokenRewardContractAddress);
+  const { contract, isLoading } = useContract(stakingContractAddress);
+
+  // Load Owned Tokens
+  const { data: ownedTokens } = useTokenBalance(tokenDropContract, address);
+
+  // Load Balance of Reward Token
+  const { data: rewardTokenBalance } = useTokenBalance(rewardTokenContract, address);
+
+  const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
+  const [claimedRewards, setClaimedRewards] = useState<BigNumber>();
+  const [stakedTokens, setStakedTokens] = useState<BigNumber>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // const signer = useSigner();
+  // const sdk = ThirdwebSDK.fromSigner(signer!);
+  // const con = sdk.getTokenDrop(tokenDropContractAddress);
+
+  const price = ethers.BigNumber.from('1000000000000000000');
+
+  useEffect(() => {
+    if (!contract || !address) return;
+    const loadClaimableRewardsAndStakedTokens = async () => {
+      const claimableRewards = await contract.call('earned', address);
+      console.log('Claimable rewards', ethers.utils.formatUnits(claimableRewards, 18));
+      const claimedRewards = await contract.call('getRewardsTokensBalance', address);
+      console.log('Claimed rewards', ethers.utils.formatUnits(claimedRewards, 18));
+      const staked = await contract.call('getStaked', address);
+      console.log('Staked tokens', ethers.utils.formatUnits(staked, 18));
+      setClaimableRewards(claimableRewards);
+      setClaimedRewards(claimedRewards);
+      setStakedTokens(staked);
+    };
+
+    loadClaimableRewardsAndStakedTokens();
+  }, [address, contract]);
+
+  const claimTokens = async (num: string | null) => {
+    if (num === null) return;
+    try {
+      setLoading(true);
+      const tx = await tokenDropContract?.claimTo(address!, +num);
+      alert('Minted tokens successfully');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const stakeTokens = async (num: string | null) => {
+    if (num === null) return;
+    try {
+      setLoading(true);
+      // const al = await tokenDropContract?.allowanceOf(address!, stakingContractAddress);
+      const sal = await tokenDropContract?.setAllowance(stakingContractAddress, +num);
+      const result = await contract?.call('stake', ethers.BigNumber.from(+num).mul(price));
+      alert('Staked tokens successfully');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const withdrawTokens = async (num: string | null) => {
+    if (num === null) return;
+    try {
+      setLoading(true);
+      const result = await contract?.call('withdraw', ethers.BigNumber.from(+num).mul(price));
+      alert('Tokens are withdrawn successfully');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const claimRewards = async () => {
+    try {
+      setLoading(true);
+      const result = await contract?.call('claimReward');
+      alert('Rewards claimed successfully');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center h-screen w-screen'>
+      {(isLoading || loading) && <Spinner />}
       {address ? (
         <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
+          <button className={style.button} onClick={disconnectWallet}>
+            Disconnect Wallet
+          </button>
+          <button
+            className={style.button}
+            onClick={() => claimTokens(prompt('How many tokens do you want to claim?', ''))}
+          >
+            Claim tokens
+          </button>
+          <button
+            className={style.button}
+            onClick={() => stakeTokens(prompt('How many tokens do you want to stake?', ''))}
+          >
+            Stake tokens
+          </button>
+          <button
+            className={style.button}
+            onClick={() => withdrawTokens(prompt('How many tokens do you want to withdraw?', ''))}
+          >
+            Withdraw tokens
+          </button>
+          <button className={style.button} onClick={() => claimRewards()}>
+            Get rewards
+          </button>
           <p>Your address: {address}</p>
+          {ownedTokens && <div>Ownned tokens (NTOK): {ownedTokens.displayValue}</div>}
+          {stakedTokens && <div>Staked tokens (NTOK): {ethers.utils.formatUnits(stakedTokens, 18)}</div>}
+          {claimableRewards && <div>Claimable rewards (CGTOK): {ethers.utils.formatUnits(claimableRewards, 18)}</div>}
+          {claimedRewards && <div>Claimed rewards (CGTOK): {ethers.utils.formatUnits(claimedRewards, 18)}</div>}
         </>
       ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
+        <div className='flex flex-col justify-center items-center h-screen w-screen'>
+          <button className={style.button} onClick={connectWithMetamask}>
+            Connect Wallet
+          </button>
+        </div>
       )}
     </div>
   );
